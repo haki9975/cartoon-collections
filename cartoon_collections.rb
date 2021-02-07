779ea95cceb051361150eda6bb673ec950af5b6e
haki9975 <elias.robert.hakim@gmail.com>
2021-02-07 12:12:33 -0500

    commit tree 4357907ff0e7a03e16b4c24d6d3f96e8f291ff82

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,16 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index {|name, index| puts "#{index+1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect {|phrase| "#{phrase.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? {|phrase| phrase.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)
+    cheese_types = ["cheddar", "gouda", "camembert"]
+ array.detect {|i| i.eql?(cheese_types[0])}
 end
